8d614e761d6e38f50d3c6eed1473a5dc4f95a894
Rafael Barrientos García <rafiprogramando@gmail.com>
2026-03-30 20:52:34 -0600

    feat: agregar kit modular y reorganizar estructura de carpetas

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -17,7 +17,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		print("estoy en el aire")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
